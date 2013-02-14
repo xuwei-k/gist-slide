6018f6d580a15ab4c01d9a7d0bc9e0dcead4e665
xuwei-k <6b656e6a69@gmail.com>
2013-02-15 04:48:13 +0900

    Revert "delete unnecessarily dependency"
    This reverts commit 56cb943a20ec45aeaa557c0c8ed948a39db2ec93.

--- src/main/scala/Markup.scala
+++ src/main/scala/Markup.scala
@@ -1,15 +1,14 @@
 package com.xuwei_k
 
-import com.tristanhunt.knockoff.DefaultDiscounter.{knockoff, toXHTML}
-import java.net.URL
-
 object Markup {
+  import pamflet.PamfletDiscounter._
+  import java.net.URL
 
   def parse(content: String, index: Int = 0) = {
     val Meta = """(?i)(HTML)""".r
 
     val slides = content.split("(?m)^!SLIDE")
-    if(slides.isEmpty) throw new Error("no slides within file %s at index %s" format(content, index))
+    if(slides.isEmpty) log("no slides within file %s at index %s" format(content, index))
     ((new xml.NodeBuffer, index) /: slides.drop(1))( (a, s) => {
       (a._1 &+ (<div class="content" id={"slide-%s" format a._2}>
        <div class="container">{
