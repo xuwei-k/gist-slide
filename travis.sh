30c3c0d3cf4c441e1a2eeb771b7027ba5cfa14ce
xuwei-k <6b656e6a69@gmail.com>
2014-04-22 01:17:21 +0900

    update

--- travis.sh
+++ travis.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-SDK=appengine-java-sdk-1.8.3
+SDK=appengine-java-sdk-1.9.2
 SDK_ZIP=$SDK.zip
-wget http://googleappengine.googlecode.com/files/$SDK_ZIP &&
+wget https://commondatastorage.googleapis.com/appengine-sdks/featured/$SDK_ZIP &&
 unzip $SDK_ZIP &&
 unset APPENGINE_SDK_HOME &&
 export APPENGINE_SDK_HOME=./$SDK &&
-wget https://raw.github.com/paulp/sbt-extras/master/sbt &&
-chmod u+x ./sbt &&
-./sbt -debug test
+sbt test
